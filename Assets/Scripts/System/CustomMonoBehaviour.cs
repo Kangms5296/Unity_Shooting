c60dfb43c14088ee078288fceecd806dd1862ea4
Kangms5296 <rlfhfhdhk@naver.com>
2021-04-15 01:08:55 +0900

    월드맵 에디터 내 grid 표시 추가

---
+++ Assets/Scripts/System/CustomMonoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMonoBehaviour : MonoBehaviour
+{
+    Transform _transform = null;
+    public new Transform transform
+    {
+        get
+        {
+            if (_transform == null)
+                _transform = base.transform;
+
+            return _transform;
+        }
+    }
+
+    /// <summary>
+    /// x, y, z Position 반환
+    /// </summary>
+    public Vector3 position3
+    {
+        get
+        {
+            if (_transform == null)
+                _transform = transform;
+
+            return _transform.position;
+        }
+    }
+
+    /// <summary>
+    /// x, z Position 반환
+    /// </summary>
+    public Vector2 position2
+    {
+        get => new Vector2(position3.x, position3.z);
+    }
+}
---
+++ Assets/Scripts/WorldMap/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : CustomMonoBehaviour
+{
+    [Header("Grid Info")]
+    [SerializeField] private float _width = 1f;
+    [SerializeField] private float _height = 1f;
+    [SerializeField] private Color _color = Color.white;
+
+    void OnDrawGizmos()
+    {  
+        if (_width <= 0 || _height <= 0)
+            return;
+
+        Gizmos.color = _color;
+
+        // 현재 scene view 에서 보고 있는 카메라 좌표를 가져옵니다.
+        Vector3 pos = transform.position;
+        for (float z = pos.z - 100f; z < pos.z + 100f; z += _height)
+        {
+            Gizmos.DrawLine(new Vector3(100f + pos.x, pos.y, Mathf.Floor(z / _height) * _height),
+            new Vector3(-100f + pos.x, pos.y, Mathf.Floor(z / _height) * _height));
+        }
+
+        for (float x = pos.x - 100f; x < pos.x + 100f; x += _width)
+        {
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / _width) * _width, pos.y, 100 + pos.z),
+            new Vector3(Mathf.Floor(x / _width) * _width, pos.y, -100 + pos.z));
+        }
+    }
+}
