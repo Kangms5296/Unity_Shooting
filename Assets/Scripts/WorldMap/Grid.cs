d4d5d688bd24768c418d3991a778372fb70a7e8f
Kangms5296 <rlfhfhdhk@naver.com>
2021-04-16 00:14:53 +0900

    Grid 격자 단위 이동 구현

--- Assets/Scripts/WorldMap/Grid.cs
+++ Assets/Scripts/WorldMap/Grid.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class Grid : CustomMonoBehaviour
 {
-    [Header("Grid Info")]
-    [SerializeField] private float _width = 1f;
-    [SerializeField] private float _height = 1f;
+    [Header("Cell Info")]
+    [SerializeField] private int _halfVertical = 10;
+    [SerializeField] private int _halfHorizontal = 10;
+
+    [Header("Cell Info")]
+    [SerializeField] private int _cellSize = 1;
     [SerializeField] private Color _color = Color.white;
 
     void OnDrawGizmos()
     {  
-        if (_width <= 0 || _height <= 0)
+        if (_cellSize <= 0
+            || _halfVertical < 0
+            || _halfHorizontal < 0)
             return;
-
         Gizmos.color = _color;
 
-        // 현재 scene view 에서 보고 있는 카메라 좌표를 가져옵니다.
-        Vector3 pos = transform.position;
-        for (float z = pos.z - 100f; z < pos.z + 100f; z += _height)
+        // 중앙 위치
+        Vector3 pos = new Vector3(Mathf.Floor(transform.position.x / _cellSize) * _cellSize
+            , Mathf.Floor(transform.position.y / _cellSize) * _cellSize
+            , Mathf.Floor(transform.position.z / _cellSize) * _cellSize);
+
+        // Vertical Grid 표시
+        for (float z = pos.z - _halfVertical; z <= pos.z + _halfVertical; z += _cellSize)
         {
-            Gizmos.DrawLine(new Vector3(100f + pos.x, pos.y, Mathf.Floor(z / _height) * _height),
-            new Vector3(-100f + pos.x, pos.y, Mathf.Floor(z / _height) * _height));
+            Gizmos.DrawLine(new Vector3(_halfHorizontal + pos.x, pos.y, Mathf.Floor(z / _cellSize) * _cellSize),
+            new Vector3(-_halfHorizontal + pos.x, pos.y, Mathf.Floor(z / _cellSize) * _cellSize));
         }
 
-        for (float x = pos.x - 100f; x < pos.x + 100f; x += _width)
+        // Horizontal Grid 표시
+        for (float x = pos.x - _halfHorizontal - 1; x < pos.x + _halfHorizontal; x += _cellSize)
         {
-            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / _width) * _width, pos.y, 100 + pos.z),
-            new Vector3(Mathf.Floor(x / _width) * _width, pos.y, -100 + pos.z));
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / _cellSize) * _cellSize + _cellSize, pos.y, _halfVertical + pos.z),
+            new Vector3(Mathf.Floor(x / _cellSize) * _cellSize + _cellSize, pos.y, -_halfVertical + pos.z));
         }
     }
 }
