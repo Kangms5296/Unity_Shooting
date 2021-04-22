47d4370bbc420f8864f80093f8e1faa8d8c5cef2
Kangms5296 <rlfhfhdhk@naver.com>
2021-04-22 23:09:49 +0900

    Editor 입력 제어 수정

--- Assets/Scripts/WorldMap/Editor/MapGridEditor.cs
+++ Assets/Scripts/WorldMap/Editor/MapGridEditor.cs
@@ -13,11 +13,6 @@ public class MapGridEditor : Editor
         _grid = (MapGrid)target;
     }
 
-    private void OnDisable()
-    {
-        _grid.EndMapEditor();
-    }
-
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -46,11 +41,6 @@ public class MapGridEditor : Editor
     {
         Event e = Event.current;
 
-        // Grid는 마우스 클릭 Event만 처리
-        if (e.type != EventType.MouseDown &&
-            e.type != EventType.MouseDrag)
-            return;
-
         // 마우스 좌표 계산
         var mousePosition = Event.current.mousePosition * EditorGUIUtility.pixelsPerPoint;
         mousePosition.y = Camera.current.pixelHeight - mousePosition.y;
@@ -58,30 +48,37 @@ public class MapGridEditor : Editor
         var Ray = Camera.current.ScreenPointToRay(mousePosition);
         if (Physics.Raycast(Ray, out RaycastHit hit))
         {
-            if (e.control)
+            
+
+            // Grid는 마우스 클릭 Event만 처리
+            if (e.type == EventType.MouseDown ||
+                e.type == EventType.MouseDrag)
             {
-                // Brick 생성
-                if (e.button == 0)
+                if (e.control)
                 {
-                    Debug.Log("Brick 생성 : "+ _grid.CalGridPosition(hit.point));
+                    // Brick 생성
+                    if (e.button == 0)
+                    {
+                        Debug.Log("Brick 생성 : " + _grid.CalGridPosition(hit.point));
 
 
-                }
-                // Brick 제거
-                else if (e.button == 1)
-                {
-                    Debug.Log("Brick 제거 : " + _grid.CalGridPosition(hit.point));
+                    }
+                    // Brick 제거
+                    else if (e.button == 1)
+                    {
+                        Debug.Log("Brick 제거 : " + _grid.CalGridPosition(hit.point));
 
 
+                    }
+
+                    // Hierachy Focus를 Grid로 유지
+                    int crtID = GUIUtility.GetControlID(FocusType.Passive);
+                    GUIUtility.hotControl = crtID;
+
+                    // Event 사용 처리
+                    Event.current.Use();
                 }
             }
-
-            // Hierachy Focus를 Grid로 유지
-            int crtID = GUIUtility.GetControlID(FocusType.Passive);
-            GUIUtility.hotControl = crtID;
-
-            // Event 사용 처리
-            Event.current.Use();
         }
     }
 }
